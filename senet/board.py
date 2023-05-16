fbd48af56b3d2ec37263276e72e740f3a0ee63de
azevedo7 <jazevedo0406@gmail.com>
2023-05-16 15:07:26 +0100

    Fix board v2

--- senet/board.py
+++ senet/board.py
@@ -36,7 +36,7 @@ class Board:
 
         row = mouse_y // SQ_SIZE
         col = mouse_x // SQ_SIZE
-        if row <= ROWS and col <= COLS:
+        if row <= ROWS-1 and col <= COLS-1:
             if self.board[row][col] != 0:
                 self.board[row][col].select()
 
