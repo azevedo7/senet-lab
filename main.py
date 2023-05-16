a1fef3a3d8d84d8fb995c5ca06f9b658ad788e2d
azevedo7 <jazevedo0406@gmail.com>
2023-05-17 00:01:14 +0100

    Move function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from senet.constants import WIDTH, HEIGHT, DARK, LIGHT
+from senet.constants import WIDTH, HEIGHT, DARK, LIGHT, SQ_SIZE
 from senet.board import Board
 from senet.sticks import Stick
 from senet.menu import menu
@@ -16,7 +16,12 @@ pygame.display.set_caption('Senet')
 #       Show valid movements
 #       Selection animation
 
+def select_piece(pos):
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
+    row = mouse_y // SQ_SIZE
+    col = mouse_x // SQ_SIZE
+    return row, col
 
 def main():
     clock = pygame.time.Clock()
@@ -25,9 +30,11 @@ def main():
 
     run = menu(screen)
 
+
     while run:
         round_over = False
         sticks = Stick()
+        selected = 0
         while not round_over:
             clock.tick(FPS)
 
@@ -36,12 +43,23 @@ def main():
                     run = False
                     round_over = True
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    board.select_piece()
-                    pass
+                    pos = pygame.mouse.get_pos()
+                    if selected == 0:
+                        row, col = select_piece(pos)
+                        piece = board.get_piece(row, col)
+                        selected = 1
+                    elif selected == 1:
+                        row, col = select_piece(pos)
+                        if piece:
+                            board.move(piece, row, col)
+                        selected = 0
+
 
             board.draw_squares(screen)
             board.print_board(screen)
             sticks.draw_sticks(screen)
+
+
 
             pygame.display.update()
 
