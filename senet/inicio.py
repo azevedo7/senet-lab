2aea81af72327e86eae099dafba8edc79ac4da5a
didi-costa <120395347+didi-costa@users.noreply.github.com>
2023-05-18 13:59:03 +0100

    inicio

--- senet/inicio.py
+++ senet/inicio.py
@@ -6,8 +6,7 @@ def inicio(screen):
   pygame.image.load("Meu projeto(1).png")
   # Defina a fonte do texto
   base_font = pygame.font.Font(None, 32)
-   menu = [{"text": "NEXT STEP", "pos": (WIDTH // 2, HEIGHT // 2 - 75)}
-           {"text": "Worked done by: César Faria, Diana Costa, João Azevedo", "pos": (WIDTH // 2, HEIGHT // 2 - 75)}]
+  menu = [{"text": "NEXT STEP", "pos": (WIDTH // 2, HEIGHT // 2 - 75)}, {"text": "Worked done by: César Faria, Diana Costa, João Azevedo", "pos": (WIDTH // 2, HEIGHT // 2 - 75)}]
   
   while True:
     for event in pygame.event.get():
