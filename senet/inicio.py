7590fa78a8c36e151bee14fe046aef4fc330733d
didi-costa <120395347+didi-costa@users.noreply.github.com>
2023-05-18 10:44:52 +0100

    tela principal

--- senet/inicio.py
+++ senet/inicio.py
@@ -1,8 +1,24 @@
 import pygame
+from menu import menu
 pygame.init
 
 def inicio(screen):
-
+  pygame.image.load("Meu projeto(1).png")
   # Defina a fonte do texto
-    base_font = pygame.font.Font(None, 32)
-    
+  base_font = pygame.font.Font(None, 32)
+   menu = [{"text": "NEXT STEP", "pos": (WIDTH // 2, HEIGHT // 2 - 75)}
+           {"text": "Worked done by: César Faria, Diana Costa, João Azevedo", "pos": (WIDTH // 2, HEIGHT // 2 - 75)}]
+  
+  while True:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Check if a menu item was clicked
+                for item in menu:
+                    if item["rect"].collidepoint(event.pos):
+                        if item["text"] == "next":
+                          menu()
+                          return True
+                            
