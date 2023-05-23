bb3ca62a4fbc428385155caa2ab98519ca617883
didi-costa <120395347+didi-costa@users.noreply.github.com>
2023-05-23 15:12:42 +0100

    user-menu

--- senet/menu.py
+++ senet/menu.py
@@ -3,6 +3,7 @@ import sys
 from senet.constants import *
 from senet.button import Button
 from senet.game import Game
+import random as rd
 
 # Loop principal
 def menu(screen):
@@ -141,3 +142,34 @@ def game_rules(screen):
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 return
+
+
+def player_name (screen):
+    jogador = ""
+    text= pygame.font.Font (None, 27)
+    activation = True
+    option_players=["Player","BOT"]
+    random_player = rd.choice(option_players)
+    write_text= text.render("The first player is:" + random_player, True, BLACK)
+    
+    while activation:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    activation=False
+                    player = "Anonymous Player" if not player else player
+                elif event.key == pygame.K_BACKSPACE:
+                    player = player [: -1]
+                else:
+                    player += event.unicode
+                    
+    screen.fill(LIGHT)
+    write_name= text.rendes("Enter your name: " + player, True, BLACK)
+    screen.blit (write_text, (40,40))
+    screen.blit (write_name, (40,80))
+    pygame.display.update()
+    
+    return  player_name
